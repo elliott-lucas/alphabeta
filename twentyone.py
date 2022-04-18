3c69ea63621120c211f08f1da0e63718b01a284c
Kyubul <59672135+Kyubul@users.noreply.github.com>
2022-04-18 12:39:38 +0100

    Added 21 as a game

---
+++ twentyone.py
@@ -0,0 +1,28 @@
+from game import Game
+
+class TwentyOne(Game):
+	def __init__(self):
+		super().__init__()
+		self.nums = []
+	
+	def getPossibleMoves(self):
+		if sum([n[1] for n in self.nums]) < 21:
+			return {1: 0, 2: 0, 3: 0}
+		else:
+			return {}
+	
+	def getEvaluation(self):
+		if sum([n[1] for n in self.nums]) >= 21:
+			return self.nums[-1][0] * float('inf')
+		else:
+			return 0
+	
+	def playMove(self, move):
+		self.nums.append([self.currentPlayer, move])
+		self.totalMoves += 1
+		
+		if self.getEvaluation() == float('inf') * self.currentPlayer:
+			self.isGameOver = True
+			self.winningPlayer = self.currentPlayer
+		else:
+			self.currentPlayer = -self.currentPlayer
